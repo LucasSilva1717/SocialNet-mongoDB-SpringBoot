88dc1744fdcb5a633a2f04d9436ae4bfbd0a5a76
Lucas Silva <nathansg20@gmail.com>
2026-05-12 16:17:20 -0300

    user insertion with post

--- src/main/java/com/social/SocialNetwork/resource/UserResource.java
+++ src/main/java/com/social/SocialNetwork/resource/UserResource.java
@@ -1,18 +1,23 @@
 package com.social.SocialNetwork.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.social.SocialNetwork.domain.User;
 import com.social.SocialNetwork.dto.UserDTO;
 import com.social.SocialNetwork.services.UserService;
+
 
 
 @RestController
@@ -34,6 +39,13 @@ public class UserResource {
         User obj = service.findById(id);
         UserDTO objDto = new UserDTO(obj);
         return ResponseEntity.ok().body(objDto);
+    }
 
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDTO) {
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
